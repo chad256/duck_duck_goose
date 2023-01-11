54f5fd51f3c779be3bb6c7c70e1866025dbc5847
Chad Brading <chadbrading@proton.me>
2023-01-10 23:15:43 -0500

    Add function to remove node from cluster

--- lib/duck_duck_goose/redis_client.ex
+++ lib/duck_duck_goose/redis_client.ex
@@ -13,6 +13,10 @@ defmodule DuckDuckGoose.RedisClient do
     GenServer.call(__MODULE__, :node_list)
   end
 
+  def remove_node do
+    GenServer.call(__MODULE__, :remove_node)
+  end
+
   def init(_) do
     {:ok, conn} = Redix.start_link()
     try_add_node(conn)
@@ -28,32 +32,51 @@ defmodule DuckDuckGoose.RedisClient do
     {:reply, node_list, %{conn: conn}}
   end
 
-  def try_add_node(conn) do
+  def handle_call(:remove_node, _, %{conn: conn}) do
+    resp =
+      conn
+      |> fetch_node_list()
+      |> parse_node_list()
+      |> do_remove_node(conn)
+
+    {:reply, resp, %{conn: conn}}
+  end
+
+  defp try_add_node(conn) do
     conn
     |> fetch_node_list()
     |> maybe_add_node(conn)
   end
 
-  def fetch_node_list(conn) do
+  defp fetch_node_list(conn) do
     Redix.command(conn, ["GET", "ddg_nodes"])
   end
 
-  def parse_node_list({:ok, nil}), do: []
-  def parse_node_list({:ok, ""}), do: []
-  def parse_node_list({:ok, nodes_json}), do: Jason.decode!(nodes_json)
+  defp update_node_list(nodes, conn) do
+    nodes_json = Jason.encode!(nodes)
+    Redix.command(conn, ["SET", "ddg_nodes", nodes_json])
+  end
 
-  def maybe_add_node({:ok, nil}, conn),
-    do: Redix.command(conn, ["SET", "ddg_nodes", Jason.encode!([node()])])
+  defp parse_node_list({:ok, nil}), do: []
+  defp parse_node_list({:ok, ""}), do: []
+  defp parse_node_list({:ok, nodes_json}), do: Jason.decode!(nodes_json)
 
-  def maybe_add_node({:ok, ""}, conn),
-    do: Redix.command(conn, ["SET", "ddg_nodes", Jason.encode!([node()])])
+  defp maybe_add_node({:ok, nil}, conn), do: update_node_list([node()], conn)
+  defp maybe_add_node({:ok, ""}, conn), do: update_node_list([node()], conn)
 
-  def maybe_add_node({:ok, nodes_json}, conn) do
+  defp maybe_add_node({:ok, nodes_json}, conn) do
     nodes = Jason.decode!(nodes_json)
 
     if Atom.to_string(node()) not in nodes do
-      nodes_json = Jason.encode!([node() | nodes])
-      Redix.command(conn, ["SET", "ddg_nodes", nodes_json])
+      update_node_list([node() | nodes], conn)
     end
+  end
+
+  defp do_remove_node([], _), do: []
+
+  defp do_remove_node(node_list, conn) do
+    node_list
+    |> List.delete(Atom.to_string(node()))
+    |> update_node_list(conn)
   end
 end
